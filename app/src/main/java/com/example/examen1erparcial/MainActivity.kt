377deb60d60d8da81c726a90d9187cb0c648fc4d
roberto <rsanchezro@educa.jcyl.es>
2025-11-10 23:16:58 +0100

    Initial commit

---
+++ app/src/main/java/com/example/examen1erparcial/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.examen1erparcial
+
+import android.os.Bundle
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        toolbar.title="LISTA DE LA COMPRA"
+        toolbar.setTitleTextColor(resources.getColor(android.R.color.white, theme))
+        setSupportActionBar(toolbar)
+        findViewById<EditText>(R.id.editTextNumberDecimal).toString().toDoubleOrNull()
+
+
+    }
+}
