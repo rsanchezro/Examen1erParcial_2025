a5d11f4847e1427cb50c7b737f4da8dc6ed93c6c
roberto <rsanchezro@educa.jcyl.es>
2025-12-08 21:28:42 +0100

    actualizado

--- app/src/main/java/com/example/examen1erparcial/modelo/Producto.kt
+++ app/src/main/java/com/example/examen1erparcial/modelo/Producto.kt
@@ -1,3 +1,6 @@
 package com.example.examen1erparcial.modelo
 
-data class Producto()
+data class Producto_Cesta(var nombre:String,
+                          var tipo:TipoProducto,
+                          var precio:Double)
+enum class TipoProducto{COMIDA,BEBIDA,LIMPIEZA_HIGIENE,OTROS}
